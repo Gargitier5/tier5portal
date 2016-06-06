8ee5757771efdbdc3bb428cd353b65b372c359de
Gargitier5 <iamgargi92@gmail.com>
2016-06-06 17:21:35 +0530

    Update Employee.php

--- application/controllers/employee_control/Employee.php
+++ application/controllers/employee_control/Employee.php
@@ -7,11 +7,7 @@ class Employee extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		if(!$this->session->userdata('uid'))
-		{
-		   redirect(base_url());
-		   exit;
-		}
+		
 		$this->load->helper('url');
 		$this->load->database();
 		$this->load->model('EmployeeModel');
